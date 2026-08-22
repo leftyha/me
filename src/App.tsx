27220bb101fe502a6ef85f96e74f49ab122390d8
leftyha <leftyha@wearehackerone.com>
2026-08-21 23:04:41 -0400

    Connect experience and technology sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Hero } from './components/sections/Hero'
 import SolutionFactory from './components/sections/SolutionFactory'
 import SystemsLab from './components/sections/SystemsLab'
+import ExperienceEngine from './components/sections/ExperienceEngine'
+import TechnologyUniverse from './components/sections/TechnologyUniverse'
 
 export default function App() {
   return (
     <main className="page">
       <Hero />
       <SolutionFactory />
+      <ExperienceEngine />
+      <TechnologyUniverse />
       <SystemsLab />
     </main>
   )

faa5287b14c2ddfb7a67859bbe70407a1b8095cf
leftyha <leftyha@wearehackerone.com>
2026-08-21 23:28:49 -0400

    wrap app in experience world

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { Hero } from './components/sections/Hero'
-import MindEngine from './components/sections/MindEngine'
-import VisualEngine from './components/sections/VisualEngine'
-import SolutionFactory from './components/sections/SolutionFactory'
-import ExperienceEngine from './components/sections/ExperienceEngine'
-import TechnologyUniverse from './components/sections/TechnologyUniverse'
-import SystemsLab from './components/sections/SystemsLab'
-import ContactEngine from './components/sections/ContactEngine'
+import { Hero } from './components/sections/Hero';
+import MindEngine from './components/sections/MindEngine';
+import VisualEngine from './components/sections/VisualEngine';
+import SolutionFactory from './components/sections/SolutionFactory';
+import ExperienceEngine from './components/sections/ExperienceEngine';
+import TechnologyUniverse from './components/sections/TechnologyUniverse';
+import SystemsLab from './components/sections/SystemsLab';
+import ContactEngine from './components/sections/ContactEngine';
+import ExperienceWorld from './engine/ExperienceWorld';
 
 export default function App() {
   return (
-    <main className="page">
-      <VisualEngine />
-      <Hero />
-      <MindEngine />
-      <SolutionFactory />
-      <ExperienceEngine />
-      <TechnologyUniverse />
-      <SystemsLab />
-      <ContactEngine />
-    </main>
-  )
+    <ExperienceWorld>
+      <main className="page">
+        <VisualEngine />
+        <Hero />
+        <MindEngine />
+        <SolutionFactory />
+        <ExperienceEngine />
+        <TechnologyUniverse />
+        <SystemsLab />
+        <ContactEngine />
+      </main>
+    </ExperienceWorld>
+  );
 }

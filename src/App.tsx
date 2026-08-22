33b4e70a6159c8126dfe9586f438f2d8f29c890d
leftyha <leftyha@wearehackerone.com>
2026-08-21 23:22:39 -0400

    build first experience flow with mind and visual engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Hero } from './components/sections/Hero'
+import MindEngine from './components/sections/MindEngine'
+import VisualEngine from './components/sections/VisualEngine'
 import SolutionFactory from './components/sections/SolutionFactory'
 import ExperienceEngine from './components/sections/ExperienceEngine'
 import TechnologyUniverse from './components/sections/TechnologyUniverse'
@@ -8,7 +10,9 @@ import ContactEngine from './components/sections/ContactEngine'
 export default function App() {
   return (
     <main className="page">
+      <VisualEngine />
       <Hero />
+      <MindEngine />
       <SolutionFactory />
       <ExperienceEngine />
       <TechnologyUniverse />

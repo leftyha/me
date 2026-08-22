c26d36b4e2b021f413fd3932af1ede7eb1881a8a
leftyha <leftyha@wearehackerone.com>
2026-08-21 23:03:34 -0400

    Create portfolio composition root

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from './components/sections/Hero'
+import SolutionFactory from './components/sections/SolutionFactory'
+import SystemsLab from './components/sections/SystemsLab'
+
+export default function App() {
+  return (
+    <main className="page">
+      <Hero />
+      <SolutionFactory />
+      <SystemsLab />
+    </main>
+  )
+}
